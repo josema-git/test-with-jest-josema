3fdfd21679a1988d2345a87e22a74de05e7ffbb8
Jose Manuel <joseman6575@gmail.com>
2024-02-07 19:45:07 -0500

    changes

--- .learn/resets/10-implementing-the-function/app.js
+++
@@ -1,9 +0,0 @@
-// One euro is:
-let oneEuroIs = {
-    "JPY": 156.5, // japan yen
-    "USD": 1.07, // us dollar
-    "GBP": 0.87, // british pound
-}
-let fromEuroToDollar= (euro)  => {  
-    return euro * oneEuroIs.USD;
-}
